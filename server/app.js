7f8bd10226548966de95f619bbd559587b314dcd
shiftaw <shiftaw.shiftaw@gmail.com>
2022-07-04 22:54:00 +0200

    add Procfile

--- server/app.js
+++ server/app.js
@@ -18,10 +18,11 @@ app.use(cookieParser())
 
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
-app.use(express.static('public'))
+app.use(express.static('build'))
 
 const mongooseDebug = true
-const mongoUri = 'mongodb://localhost:27017/test'
+const mongoUri =
+  'mongodb+srv://shiftaw:Ict4rd2012@cluster0.jpxr5mv.mongodb.net/data?authSource=admin&replicaSet=atlas-uu3ocy-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
 mongoose.connect(mongoUri).then((res) => {
   console.log('connected to db  ', mongoUri)
 })
@@ -44,7 +45,7 @@ app.use(function (err, req, res, next) {
 // mount all routes on /api path
 
 app.use('/victim', add_route)
-
-app.listen(4000, () => {
+const port = process.env.PORT || 4000
+app.listen(port, () => {
   console.log('App started', 4000)
 })
